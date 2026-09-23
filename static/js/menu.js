7ac77cf5a56db8d14b9c52bd1a6b6caa2fcf423a
NitrogenDev20 <321542515+NitrogenDev20@users.noreply.github.com>
2026-09-23 19:55:21 -0400

    idk man

--- static/js/menu.js
+++ static/js/menu.js
@@ -121,7 +121,7 @@ if (uiLayer) {
         showScreen("settings");
         break;
       case "trial":
-        window.location.href = "/trial";
+        window.location.href = "trial.html";
         break;
       case "credits":
         showScreen("credits");
